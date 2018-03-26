2780345c7538b0dfbdecc619e4036bc9896a2d64
Andrei Bushev <morgan9757@gmail.com>
2018-03-26 23:51:46 +0300

    fix tabs & comments

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ def parse(equation):
 	equation_temp = equation.split('=')
 	equation_temp[1]=simplify(equation_temp[1])
 	if equation_temp[1]>0:
-		equation_temp[1]=str(equation_temp[1])
+		equation_temp[1]=str(equation_temp[1])#bad
 		equation_done='-'.join(equation_temp)
-		#print(equation_done)
 	else:
-		equation_temp[1]=str(abs(equation_temp[1]))
+		equation_temp[1]=str(abs(equation_temp[1])) #bad
 		equation_done='+'.join(equation_temp)
 	return equation_done
 
